8bbc76a6f76e62b46df2a5139f16b1259a71a268
bachar157 <bachar.alhamada76@gmail.com>
2023-11-20 19:09:46 -0600

    first commit

---
+++ assets/script/index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+import {select, getElement, onEvent} from "./utilty.js";
+let curntlyTIme;
+
+function updateCurrentTime() {
+  const now = new Date();
+  document.getElementById('curntly-time').textContent = curntlyTIme;
+  const hours = now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+ 
+  curntlyTIme=(`${hours}:${minutes}`)
+  if(usermin===minutes && userhour===hours){
+    playAlarm()
+  }
+}
+
+// Update the time every second
+setInterval(updateCurrentTime, 1000);
+
+// Function to validate and set alarm
+function setAlarm() {
+  const hour = getElement('hour').value;
+  const minute = getElement('minute').value;
+  const button = document.getElementById('button');
+  const feedbackElement = document.querySelector('.setting-analarm');
+    
+  // Check if hour and minute are numbers and within range
+  if (!isNaN(hour) && hour >= 0 && hour < 24 && !isNaN(minute) && minute >= 0 && minute < 60) {
+      // Correct values - set alarm and update UI
+      feedbackElement.textContent = `Alarm set for ${hour.padStart(2, '0')}:${minute.padStart(2, '0')}`;
+      button.classList.remove('error');
+      if(minute===minutes && hour===hours){
+        playAlarm()
+      }
+  } else {
+      // Incorrect values - show error and update UI
+      feedbackElement.textContent = 'Please add a correct value';
+      button.classList.add('error');
+  }
+}
+setAlarm()
+// Add click event listener to the button
+document.getElementById('button').addEventListener('click', setAlarm);
+
+setInterval(updateClock, 1000);
+updateClock(); // Initialize clock immediately
+
+function playAlarm() {
+  const sound = new Audio('./assets/sound/digital-alarm-beeping-slava-pogorelsky-1-00-06.mp3')
+  sound.type='audio/mp3';
+  sound.play();
+  alarmTime = null;
+}
